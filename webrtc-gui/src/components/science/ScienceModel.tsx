97badf8be6c9ef6c2fafc9c8b4b1dc51a40f556e
JonTran04 <jontran04@gmail.com>
2026-03-18 16:47:37 +1100

    basic sciencemodel

--- webrtc-gui/src/components/science/ScienceModel.tsx
+++ webrtc-gui/src/components/science/ScienceModel.tsx
@@ -1,1 +1,43 @@
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+"use client";
+
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+import * as THREE from 'three'
+
+export function ScienceModel(props: any) {
+  // 1. Fix the path to match where your file actually is
+  const { nodes, materials } = useGLTF('/models/SciencePayload.gltf') as any
+
+  // 2. Define one shared Black Material
+  const blackMaterial = (
+    <meshStandardMaterial
+      color="#000000"
+      roughness={0.2}
+      metalness={0.8}
+    />
+  )
+
+  return (
+    <group {...props} dispose={null}>
+      <group position={[9.532, 1.688, 8.342]} scale={0.329}>
+        {/* We override the materials here by putting our blackMaterial inside the mesh */}
+        <mesh geometry={nodes.Cube016.geometry}>{blackMaterial}</mesh>
+        <mesh geometry={nodes.Cube016_1.geometry}>{blackMaterial}</mesh>
+
+        <group position={[-14.148, 20.787, -19.927]} scale={2.6}>
+          <mesh geometry={nodes.Cylinder005.geometry}>{blackMaterial}</mesh>
+          <mesh geometry={nodes.Cylinder005_1.geometry}>{blackMaterial}</mesh>
+        </group>
+
+        <mesh geometry={nodes.MicroscopeStepper.geometry} position={[-46.328, -1.772, 25.503]} rotation={[-Math.PI, 0, 0]} scale={8.482}>
+          {blackMaterial}
+          <mesh geometry={nodes.MicroscopeSwivel.geometry} position={[0.004, 0.695, 0.065]} rotation={[Math.PI, 0, 0]} scale={[0.358, 0.398, 0.358]}>
+            {blackMaterial}
+          </mesh>
+        </mesh>
+      </group>
+    </group>
+  )
+}
+
+useGLTF.preload('/models/SciencePayload.gltf')
--- webrtc-gui/src/pages/ScienceControl.tsx
+++ webrtc-gui/src/pages/ScienceControl.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import AugerWidget from "@/components/science/AugerWidget"
+import React, { Suspense } from "react";
 import DrillWidget from "@/components/science/DrillWidget"
+import AugerWidget from "@/components/science/AugerWidget"
 import HeatpadWidget from "@/components/science/HeatpadWidget"
 import MicroscopeCamera from "@/components/science/MicroscopeCamera"
 import styles from "./Science.module.scss"
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, OrbitControls, Stage } from "@react-three/drei"
+import { ScienceModel } from "@/components/science/ScienceModel"; // Import the component above
+
+function Model() {
+  // Ensure this file is at: public/models/SciencePayload.gltf
+  const { scene } = useGLTF('/models/SciencePayload.gltf');
+  return <primitive object={scene} />;
+}
 
 const ScienceControl: React.FC = () => {
   return (
@@ -20,6 +29,17 @@ const ScienceControl: React.FC = () => {
         <HeatpadWidget />
 
         <MicroscopeCamera />
+
+        <div style={{ width: '100%', height: '400px', background: '#111', borderRadius: '8px' }}>
+          <Canvas camera={{ position: [20, 20, 20], fov: 45 }}>
+            <Suspense fallback={null}>
+              <Stage environment="city" intensity={0.5}>
+                <ScienceModel />
+              </Stage>
+            </Suspense>
+            <OrbitControls />
+          </Canvas>
+        </div>
       </div>
     </div >
   );
