111769ef52d77d22b4c058075abe3e2bc0c5bc15
JonTran04 <jontran04@gmail.com>
2026-03-18 21:31:00 +1100

    Science Model

--- webrtc-gui/src/components/science/ScienceModel.tsx
+++ webrtc-gui/src/components/science/ScienceModel.tsx
@@ -5,36 +5,40 @@ import { useGLTF } from '@react-three/drei'
 import * as THREE from 'three'
 
 export function ScienceModel(props: any) {
-  // 1. Fix the path to match where your file actually is
   const { nodes, materials } = useGLTF('/models/SciencePayload.gltf') as any
-
-  // 2. Define one shared Black Material
-  const blackMaterial = (
-    <meshStandardMaterial
-      color="#000000"
-      roughness={0.2}
-      metalness={0.8}
-    />
-  )
 
   return (
     <group {...props} dispose={null}>
-      <group position={[9.532, 1.688, 8.342]} scale={0.329}>
-        {/* We override the materials here by putting our blackMaterial inside the mesh */}
-        <mesh geometry={nodes.Cube016.geometry}>{blackMaterial}</mesh>
-        <mesh geometry={nodes.Cube016_1.geometry}>{blackMaterial}</mesh>
+      {/* Main offset and scale from your Blender export */}
+      <group position={[2.941, 0.521, 2.574]} scale={0.102}>
 
+        {/* Main body meshes - using original materials */}
+        <mesh geometry={nodes.Cube016.geometry} material={materials['Plastic ']} />
+        <mesh geometry={nodes.Cube016_1.geometry} material={materials['Metal ']} />
+        <mesh geometry={nodes.Cube016_2.geometry} material={materials['Alu Beam ']} />
+
+        {/* Cylinder / Drill component */}
         <group position={[-14.148, 20.787, -19.927]} scale={2.6}>
-          <mesh geometry={nodes.Cylinder005.geometry}>{blackMaterial}</mesh>
-          <mesh geometry={nodes.Cylinder005_1.geometry}>{blackMaterial}</mesh>
+          <mesh geometry={nodes.Cylinder005.geometry} material={materials['Metal ']} />
+          <mesh geometry={nodes.Cylinder005_1.geometry} material={materials['Plastic ']} />
+          <mesh geometry={nodes.Cylinder005_2.geometry} material={materials.Logo} />
         </group>
 
-        <mesh geometry={nodes.MicroscopeStepper.geometry} position={[-46.328, -1.772, 25.503]} rotation={[-Math.PI, 0, 0]} scale={8.482}>
-          {blackMaterial}
-          <mesh geometry={nodes.MicroscopeSwivel.geometry} position={[0.004, 0.695, 0.065]} rotation={[Math.PI, 0, 0]} scale={[0.358, 0.398, 0.358]}>
-            {blackMaterial}
-          </mesh>
-        </mesh>
+        {/* Stepper / Microscope component */}
+        <group position={[-46.328, -1.772, 25.503]} rotation={[-Math.PI, 0, 0]} scale={8.482}>
+          <mesh geometry={nodes.Cube020.geometry} material={materials['Plastic ']} />
+          <mesh geometry={nodes.Cube020_1.geometry} material={materials['Metal ']} />
+
+          {/* The Swivel part */}
+          <mesh
+            geometry={nodes.MicroscopeSwivel.geometry}
+            material={materials['Plastic ']}
+            position={[0.004, 0.695, 0.065]}
+            rotation={[Math.PI, 0, 0]}
+            scale={[0.358, 0.398, 0.358]}
+          />
+        </group>
+
       </group>
     </group>
   )
